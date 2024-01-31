7260cd04e1ed98f31746a36ca4f23da790dd1c77
samikshya <samikshyakharel405@gmail.com>
2024-02-01 01:34:44 +1100

    edit landing page

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Component/Header'
-import './landingPage.css'
+import './landingpage.css'
 import { Container } from 'react-bootstrap'
 import Banner from '../Component/Banner'
 
