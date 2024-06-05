89033596a541c1c1bf98c1690b80add57f3eabb6
samikshya <samikshyakharel405@gmail.com>
2024-06-05 21:35:48 +1000

    css

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -7,7 +7,7 @@ import { Linkedin, Github, Envelope } from "react-bootstrap-icons";
 const Banner = () => {
   return (
     <div className="top">
-      <div className="d-flex align-items-center justify-content-between  banner">
+      <div className="  banner">
         <div className="d-flex flex-column gap-3 content">
           {/* <h3 className="custom-color">Hello !</h3> */}
           <h1 className="title">Front-end React Developer</h1>
